27984e7e03b18c1b65b68a0698ae97862f29cef3
Denis Korablev <info@codeweber.com>
2025-09-15 01:57:46 +0300

    staff

--- functions.php
+++ functions.php
@@ -170,3 +170,85 @@ function codeweber_make_staff_additional_columns_sortable($columns)
     return $columns;
 }
 add_filter('manage_edit-staff_sortable_columns', 'codeweber_make_staff_additional_columns_sortable');
+
+
+
+
+/**
+ * Шорткод для вывода сотрудников (staff) с сортировкой по дате создания
+ */
+function codeweber_staff_shortcode($atts)
+{
+    // Атрибуты шорткода
+    $atts = shortcode_atts(array(
+        'posts_per_page' => -1,
+        'orderby' => 'date',  // Сортировка по дате создания
+        'order' => 'DESC'     // По умолчанию новые сначала
+    ), $atts);
+
+    // Аргументы для WP_Query
+    $args = array(
+        'post_type' => 'staff',
+        'posts_per_page' => $atts['posts_per_page'],
+        'orderby' => $atts['orderby'],
+        'order' => $atts['order']
+    );
+
+    $query = new WP_Query($args);
+    $output = '';
+
+    if ($query->have_posts()) {
+        $output .= '<div class="row gx-md-5 gy-5">';
+
+        while ($query->have_posts()) {
+            $query->the_post();
+            $post_id = get_the_ID();
+
+            // Получаем метаданные
+            $position = get_post_meta($post_id, '_staff_position', true);
+            $name = get_post_meta($post_id, '_staff_name', true);
+            $surname = get_post_meta($post_id, '_staff_surname', true);
+            $regions = get_post_meta($post_id, '_staff_regions', true);
+            $thumbnail = get_the_post_thumbnail_url($post_id, 'full');
+
+            // Формируем полное имя
+            $full_name = trim($name . ' <span class="text-uppercase">' . $surname . '</span>');
+
+            $output .= '
+            <div class="col-md-4">
+                <a href="' . get_permalink() . '" class="swiper-slide" data-cue="slideInDown">
+                    <figure class="lift">';
+
+            if ($thumbnail) {
+                $output .= '<img src="' . esc_url($thumbnail) . '" alt="' . esc_attr($full_name) . '" />';
+            } else {
+                $output .= '<img src="/wp-content/uploads/placeholder.jpg" alt="' . esc_attr($full_name) . '" />';
+            }
+
+            $output .= '
+                        <div class="caption-wrapper p-7">
+                            <div class="caption bg-matte-color mt-auto label-u text-neutral-50 px-4 py-2">' . esc_html($position) . '</div>
+                        </div>
+                    </figure>
+                    <div class="team-item-content text-dark mt-4">
+                        <h3 class="h4">' . $full_name . '</h3>';
+
+            if ($regions) {
+                $output .= '<div class="label-u">' . esc_html($regions) . '</div>';
+            }
+
+            $output .= '
+                    </div>
+                </a>
+            </div>';
+        }
+
+        $output .= '</div>';
+        wp_reset_postdata();
+    } else {
+        $output = '<p>No employees found</p>';
+    }
+
+    return $output;
+}
+add_shortcode('staff_grid', 'codeweber_staff_shortcode');
