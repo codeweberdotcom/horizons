7e34974fa8e6d2886b7f5d00400e3d64da7b5075
Denis Korablev <info@codeweber.com>
2025-09-17 20:16:01 +0300

    fix awards

--- templates/content/single-staff.php
+++ templates/content/single-staff.php
@@ -4,4 +4,4 @@
    </div>
 </div>
 
-<?php echo do_shortcode('[awards_grid posts_per_page="6" columns="3" columns_md="3" columns_sm="1"]'); ?>
+<?php echo do_shortcode('[awards_grid posts_per_page="5" columns="3" columns_md="3" columns_sm="1"]'); ?>
