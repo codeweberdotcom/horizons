4f62cec4c0d64f24de731719a31181fdf878c848
Denis Korablev <info@codeweber.com>
2025-10-02 13:04:17 +0300

    blog sidebar

--- sidebar-left.php
+++ sidebar-left.php
@@ -1,28 +1,40 @@
 <?php
 global $opt_name;
 
-$post_type = get_post_type();
+$post_type = universal_get_post_type();
 $post_type_lc = strtolower($post_type);
 
 $sidebar_position = get_sidebar_position($opt_name);
 
-
 // Левый сайдбар
-if ($sidebar_position === 'left' && is_active_sidebar($post_type)) { ?>
-   <aside class="col-xl-4 sidebar sticky-sidebar mt-md-0 py-14 d-none d-xl-block">
-      <?php
-      do_action('codeweber_before_sidebar', $post_type);
-      dynamic_sidebar($post_type);
-      do_action('codeweber_after_sidebar', $post_type);
-      ?>
-   </aside>
-   <?php
-} else {
-   if ($sidebar_position === 'left' && !is_active_sidebar($post_type)) { ?>
+if ($sidebar_position === 'left') {
+   if ($post_type === 'post') {
+      // Используем стандартный сайдбар WordPress с нужными классами
+?>
       <aside class="col-xl-4 sidebar sticky-sidebar mt-md-0 py-14 d-none d-xl-block">
          <?php
-         do_action('codeweber_after_widget', $post_type); ?>
+         do_action('codeweber_before_sidebar', 'sidebar-1');
+         get_sidebar();
+         do_action('codeweber_after_sidebar', 'sidebar-1');
+         ?>
       </aside>
+      <?php
+   } else {
+      // Используем кастомный сайдбар для других типов записей
+      if (is_active_sidebar($post_type)) { ?>
+         <aside class="col-xl-4 sidebar sticky-sidebar mt-md-0 py-14 d-none d-xl-block">
+            <?php
+            do_action('codeweber_before_sidebar', $post_type);
+            dynamic_sidebar($post_type);
+            do_action('codeweber_after_sidebar', $post_type);
+            ?>
+         </aside>
+      <?php
+      } else { ?>
+         <aside class="col-xl-4 sidebar sticky-sidebar mt-md-0 py-14 d-none d-xl-block">
+            <?php do_action('codeweber_after_widget', $post_type); ?>
+         </aside>
 <?php
+      }
    }
 }
--- sidebar-right.php
+++ sidebar-right.php
@@ -7,21 +7,34 @@ $post_type_lc = strtolower($post_type);
 $sidebar_position = get_sidebar_position($opt_name);
 
 // Правый сайдбар
-if ($sidebar_position === 'right' && is_active_sidebar($post_type)) { ?>
-   <aside class="col-xl-4 sidebar sticky-sidebar mt-md-0 py-14 d-none d-xl-block">
-      <?php
-      do_action('codeweber_before_sidebar', $post_type);
-      dynamic_sidebar($post_type);
-      do_action('codeweber_after_sidebar', $post_type);
-      ?>
-   </aside>
-   <?php
-} else {
-   if ($sidebar_position === 'right' && !is_active_sidebar($post_type)) { ?>
+if ($sidebar_position === 'right') {
+   if ($post_type === 'post') {
+      // Используем стандартный сайдбар WordPress с нужными классами
+?>
       <aside class="col-xl-4 sidebar sticky-sidebar mt-md-0 py-14 d-none d-xl-block">
          <?php
-         do_action('codeweber_after_widget', $post_type); ?>
+         do_action('codeweber_before_sidebar', 'sidebar-1');
+         get_sidebar();
+         do_action('codeweber_after_sidebar', 'sidebar-1');
+         ?>
       </aside>
+      <?php
+   } else {
+      // Используем кастомный сайдбар для других типов записей
+      if (is_active_sidebar($post_type)) { ?>
+         <aside class="col-xl-4 sidebar sticky-sidebar mt-md-0 py-14 d-none d-xl-block">
+            <?php
+            do_action('codeweber_before_sidebar', $post_type);
+            dynamic_sidebar($post_type);
+            do_action('codeweber_after_sidebar', $post_type);
+            ?>
+         </aside>
+      <?php
+      } else { ?>
+         <aside class="col-xl-4 sidebar sticky-sidebar mt-md-0 py-14 d-none d-xl-block">
+            <?php do_action('codeweber_after_widget', $post_type); ?>
+         </aside>
 <?php
+      }
    }
 }
