f8fb57d4f6ab02a504bdfa6990176ccc47eb4fc1
Denis Korablev <info@codeweber.com>
2026-02-20 13:51:43 +0300

    ++

---
+++ templates/archives/legal/legal_1.php
@@ -0,0 +1,32 @@
+<?php $card_radius = getThemeCardImageRadius(); ?>
+<article id="<?= $post->post_name; ?>" <?php post_class('post'); ?>>
+   <div class="card<?php echo $card_radius ? ' ' . esc_attr($card_radius) : ''; ?>">
+      <figure class="card-img-top overlay overlay-1 hover-scale">
+         <a href="<?php the_permalink(); ?>">
+            <?php
+            the_post_thumbnail(
+               'codeweber_single',
+               array(
+                  'class' => 'img-fluid mb-3',
+                  'alt' => get_the_title(),
+               )
+            );
+            ?></a>
+         <figcaption>
+            <h5 class="from-top mb-0"><?php esc_html_e('Read More', 'codeweber'); ?></h5>
+         </figcaption>
+      </figure>
+
+      <div class="card-body">
+         <div class="post-header">
+            <h3 class="post-title mt-1 mb-0"><a class="link-dark" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+         </div>
+         <!-- /.post-header -->
+         <div class="post-content">
+            <p><?php the_excerpt(); ?></p>
+         </div>
+         <!-- /.post-content -->
+         <a href="<?php the_permalink(); ?>" class="hover more1 label-u text-charcoal-blue mb-5"><?php esc_html_e('Go', 'codeweber'); ?></a>
+      </div>
+   </div>
+</article> <!-- #post-<?php the_ID(); ?> -->
